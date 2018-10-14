b503f69333a69185fb0f4a196733db04887de4b5
Marissa Allen <allenmm@augsburg.edu>
2018-10-14 01:12:11 -0500

    Added comments and modified the file to make unit testing easier.

--- allen_take_off_time.py
+++ allen_take_off_time.py
@@ -1,47 +1,45 @@
-# allen_take_off_time.py
-# Author: Marissa Allen
 """
-    Insert file description here.
+allen_take_off_time.py
+author: Marissa Allen
+
+allen_take_off_time is a program that takes in a file pathname the user submits from the terminal, reads the file lines, stores
+the lines in a list as strings split by commas, and passes those into another function/class to be...
 """
 
 import sys
 from queue import PriorityQueue
-
+import priority_queue
 
 def file_read(user_file):
     """
-        Reads a file for the airplane requests. When a text file is submitted by the user...
-        
+    Reads the file for the airplane requests. When a text file is submitted by the user the program tries to open
+    the file. A file object is created when the file successfully opens...
+    If the file fails to open, a message will print to the terminal, saying that the program could not read the file
+    and ask the user to submit another file.
+
+    :param user_file: contains a command line argument file pathname that the user submitted.
+    :return: Returns the list to the function call.
     """
-    
+
     try:
-        file = open(user_file, 'rt')      # Tries to open the user's text file.
-    except Exception:                     # If the file open fails it tells the user to submit another text file.
+        file = open(user_file, 'r')          # Tries to open the user's text file.
+    except Exception:                        # If the file open fails it tells the user to submit another text file.
         print("\nCould not read the file or directory:", sys.argv[1])
-        print("Please submit a valid text file to the program.")
+        print("Please submit a valid text file to the program.\n")
         print("Example: test.txt is a valid file name.\n")
-        sys.exit()
-    readlines = file.read().splitlines()                          # Reads the content of the text file and returns a list with all the lines in a string.
-    file.close()                                                  # Flushes unwritten information from the file and closes it.
-    schedule_list = [line.split(", ") for line in readlines]      # Seperates strings into a list containing one line per element.
-    print(schedule_list)                                          # Test print list.
-    return schedule_list                                          # Returns the list to the function call.
-
-
-def plane_queue(self):
-    """ 
-    Pydoc comments go under all functions. 
-    """
-
+        sys.exit()                                              # Exits Python, program does not raise SystemExit and returns.
+    readlines = file.read().splitlines()                        # Reads content of the text file and returns a list with all the lines in a string.
+    file.close()                                                # Flushes unwritten information from the file and closes it.
+    schedule_list = [line.split(", ") for line in readlines]    # Seperates strings into a list containing one line per element.
+    return schedule_list
 
 
 def main(user_file):
     """
-    Pydoc comments go under all functions.
+    Passes the file pathname to the file_read function and stores its return value for future use.
+    :param user_file: contains a command line argument file pathname that the user submitted.
     """
-    # print("This is a test of the emergency broadcast system. ")
-    schedule_list = file_read(user_file)
+    plane_list = file_read(user_file)   # Stores value of file.
 
-
-if __name__ == '__main__':          # Running the code from this module, this main, if we're running the program directly from this file.
-    main(sys.argv[1])                          # Calls main function.
+if __name__ == '__main__':  # Runs if we're running the program directly from this file.
+    main(sys.argv[1])       # Calls main function, passes in user text file.
