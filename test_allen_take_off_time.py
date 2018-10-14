2151a48939bf637369338283b5fc7267c669be14
Marissa Allen <allenmm@augsburg.edu>
2018-10-14 17:48:48 -0500

    Added a new test for if the queue is empty and the get function is called.

--- test_allen_take_off_time.py
+++ test_allen_take_off_time.py
@@ -20,12 +20,12 @@ def test_main():
 
     q = priority_queue.MyPriorityQueue()
     q.put((1, 5, 5))
-    q.put((2, 2, 7))
+    q.put((2, 2, 5))
     q.put((2, 1, 5))
     test1 = q.empty()
     print("Should be false: ", test1)
-    print("Ordered by second element test. Below should be (2, 1, 5) (2, 2, 7) (1, 5, 5):")
-    print(q.get(), q.get(), q.get(),"\n")
+    print("Ordered by second element test. Below should be (2, 1, 5) (2, 2, 5) (1, 5, 5):")
+    print(q.get(), q.get(), q.get(), "\n")
     test2 = q.empty()
     print("Should be true: ", test2)
     q.put(('delta 70', 1, 3))
@@ -35,12 +35,14 @@ def test_main():
     q.put((2, 1, 5))
     q.put(('ula 80', 1, 5))
     q.put((2, 1, 5))
+    print(q.queue)
     print("Ordered by second and third element test. Below should be: ('ula 7', 1, 2), ('delta 70', 1, 3), (2, 1, 5), "
           "('ula 80', 1, 5), (2, 1, 5), (3, 2, 5), (2, 2, 7)")
     while not q.empty():
         print(q.get())
     test3 = q.empty()
     print("Should be true: ", test3)
+    print("Test for if a get() is called after queue is empty. Should say 'The queue is empty!': ", q.get())
 
     actual = allen_take_off_time.file_read(sys.argv[1])       # for if file accept test passes.
     print("\nShould print a nested list of the text file containing one string line per element: ", actual)
