77d441e22973d7f7cced7b2c36b4ea33a06bcffe
Marissa Allen <allenmm@augsburg.edu>
2018-10-21 19:31:36 -0500

    Added the __str__ function and also assigned __repr__ as __str__ to be able to print the class.

--- airstrip_schedule.py
+++ airstrip_schedule.py
@@ -6,13 +6,18 @@ A class that will, in the future, keep track of the information needed to schedu
 """
 
 
-class AirstripSchedule:     # Declaring placeholder variables.
-    def __init__(self, plane_id, submission_time, requested_start, requested_duration, actual_start, actual_end):
+class AirstripSchedule:
+    def __init__(self, plane_id, submission_time, requested_start, requested_duration):
         self.plane_id = plane_id
         self.submission_time = submission_time
         self.requested_start = requested_start
         self.requested_duration = requested_duration
-        self.actual_start = 0       # initialized, will start at time slot 0 if !q.empty().
-        self.actual_end = 0         #  and increment counter.
+        self.actual_start = 0
+        self.actual_end = 0
 
+    def __str__(self):
+        return self.plane_id + "," + self.submission_time + "," + self.requested_start + "," + str(self.requested_duration) \
+               + "," + str(self.actual_start) + "," + str(self.actual_end)
+
+    __repr__ = __str__          #
 
