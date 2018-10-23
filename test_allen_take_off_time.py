0e814df1f276a9ca94ad204f0647bc24de45f0f3
Marissa Allen <allenmm@augsburg.edu>
2018-10-23 16:07:30 -0500

    Updated formatting of this file to make it easier to see which tests are which.

--- test_allen_take_off_time.py
+++ test_allen_take_off_time.py
@@ -24,7 +24,7 @@ def test_main():
     q.enqueue(2, 1, 5, 0)
     test1 = q.empty()
     print("Should be false: ", test1)
-    print("Ordered by second element test. "
+    print("\nOrdered by second element test. "
           "Below should be (2, 1, 5, 0) (2, 2, 5, 0) (1, 5, 5, 0):")
     print(q.dequeue(), q.dequeue(), q.dequeue(), "\n")
     test2 = q.empty()
@@ -36,23 +36,23 @@ def test_main():
     q.enqueue(2, 1, 5, 0)
     q.enqueue('ual 80', 1, 5, 0)
     q.enqueue(2, 1, 5, 0)
-    print("Peek should be '('ual 7', 1, 2, 0)':", q.peek())
-    print("Ordered by second and third element test. "
-          "Below should be: ('ual 7', 1, 2, 0), ('delta 70', 1, 3, 0), "
+    print("\nPeek should be '('ual 7', 1, 2, 0)':", q.peek())
+    print("\nOrdered by second and third element test. "
+          "\nBelow should be: ('ual 7', 1, 2, 0), ('delta 70', 1, 3, 0), "
           "(2, 1, 5, 0), ('ual 80', 1, 5, 0), (2, 1, 5, 0), (3, 2, 5, 0), "
           "(2, 2, 7, 0)")
     while not q.empty():
         print(q.dequeue())
     test3 = q.empty()
-    print("Should be true: ", test3)
-    print("Test for if dequeue() is called after queue is empty. "
-          "Should say 'The queue is empty': ", q.dequeue())
-    print("Test for if peek() is called after queue is empty. "
-          "Should say 'Cannot peek when queue is empty':", q.peek())
+    print("\nShould be true: ", test3)
+    print("\nTest for if dequeue() is called after queue is empty. "
+          "\nShould say 'The queue is empty': ", q.dequeue())
+    print("\nTest for if peek() is called after queue is empty. "
+          "\nShould say 'Cannot peek when queue is empty':", q.peek())
 
     class_test_list = [['Delta 170', '9', '10', '2'],
                        ['UAL 7','1', '2', '1']]
-    print("\nTest to see if it accepts nested list of string objects and "
+    print("\nTest to see if it accepts nested list of string objects and"
           " adds them to the class",
           "\nShould be [Delta 170,9,10,2,0,0, UAL 7,1,2,1,0,0]: ",
           allen_take_off_time.class_requests(class_test_list))
@@ -63,7 +63,6 @@ def test_main():
           "('Delta 170', '9', '10', '2')]: ", )
     pq_test_list = allen_take_off_time.class_requests(class_test_list)
     print(allen_take_off_time.sorted_requests(pq_test_list))
-
 
 
 if __name__ == '__main__':
